3240992c3ee310387d8013dec89dff9a65a7f90f
Costea Cristian <118203410+CosteaCristian1@users.noreply.github.com>
2023-03-26 20:34:07 +0300

    Add files via upload

---
+++ dfa.py
@@ -0,0 +1,37 @@
+with open('dfa.txt') as file:
+    *tranz, fin = file
+
+stari = dict()
+
+for i in tranz:
+    trzs = i.strip().split(' ')
+    print(trzs)
+    try:
+        stari[trzs[0]][trzs[1]] = trzs[2]
+    except:
+        stari[trzs[0]] = dict()
+        stari[trzs[0]][trzs[1]] = trzs[2]
+print(stari)
+stare_start='q0'
+stare_finala=set(fin.strip().split(' '))
+print(stare_finala)
+def read():
+    return input("Introdu cuvant: ")
+
+def dfa(input):
+    stare_curenta = stare_start
+    drum=[stare_curenta]
+    print(stare_curenta)
+    for chestie in input:
+        stare_curenta = stari[stare_curenta].get(chestie)
+        if stare_curenta is None:
+            return False
+        drum.append(stare_curenta)
+    return [stare_curenta in stare_finala,drum]
+
+input = read()
+if dfa(input)[0]:
+    print(f"Cuvantul '{input}' este acceptat, drumul este {dfa(input)[1]}")
+else:
+    print(f"Cuvantul '{input}' nu este acceptat")
+
